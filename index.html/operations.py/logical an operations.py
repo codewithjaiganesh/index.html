ab1481cdb82d2321d2eb9f16a4e748a1c90f5a36
codewithjaiganesh <jaiganeshreddy69@gmail.com>
2025-04-28 14:38:26 +0530

    click

---
+++ index.html/operations.py/logical an operations.py
@@ -0,0 +1,20 @@
+# and logical AND 
+a = True 
+b = False
+im = a and b  
+print(im)
+
+# or logical OR 
+X = True 
+Y = False
+result = X or Y 
+print(result)
+
+# not logical NOT 
+U = True 
+V = False 
+to = not U
+print(to)
+
+# This is all about the logical and operations 
+
